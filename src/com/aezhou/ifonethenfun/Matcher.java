6ab5bfe53b406ac334fd2c21d94d42f9ab4c46c1
Amanda Zhou <aezhou@mit.edu>
2014-12-03 22:59:29 -0500

    Updated comments for Matcher

--- src/com/aezhou/ifonethenfun/Matcher.java
+++ src/com/aezhou/ifonethenfun/Matcher.java
@@ -8,6 +8,14 @@ import java.util.*;
  * MATCHING_THRESHOLD is a percentage of the company's employees, where X% if the company is "fully matched," we start
  *   the matching process over. (Fully matched means a person has been matched with all possible team mates, across all
  *   teams. Starting the matching process over means that an employee will begin trying to match with team mates again.
+ *
+ *   stillMatching is the set of employees who still have teammates left to match with
+ *   done is the set of employees who have met with all of their teammates
+ *   potentialMatches maps an employee to all teammates with whom they have not been matched with yet (across multiple
+ *       rounds of matching)
+ *   alreadyMatched maps an employee to all teammates with whom they have already been matched with (across multiple
+ *       rounds of matching)
+ *
  */
 public class Matcher {
     private Organization organization;
@@ -24,6 +32,10 @@ public class Matcher {
         resetMatchingData();
     }
 
+    /**
+     * Creates 1+1 pairing for a single time (week, or whatever the frequency is)
+     * @return a set of matchings, or OneOnOne
+     */
     public Set<OneOnOne> getNextMatches(){
         // If enough people have been fully matched, then start a new cycle of matches
         if (done.size() >= MATCHING_THRESHOLD * organization.getEmployees().size()) {
@@ -44,6 +56,7 @@ public class Matcher {
         Set<Person> matchedInThisRound = new HashSet<Person>();
         Set<OneOnOne> matches = new HashSet<OneOnOne>();
 
+        // Create matchings (if possible) for all members who need still have teammates left to match with
         for(Person p : beingMatchedThisRound) {
             if (!matchedInThisRound.contains(p)) {
                 OneOnOne potentialMatch = createMatchWithUnmatchedTeammate(p, matchedInThisRound);
@@ -58,7 +71,8 @@ public class Matcher {
         unMatchedInThisRound.removeAll(matchedInThisRound);
         createRandomMatches(unMatchedInThisRound, matches);
 
-        // With this set of matches, if people have no potential matches left, move from stillMatching to done
+        // From the people who were matched in non-random matches, if someone has no one left to potentially match with,
+        //    move the person from stillMatching to done
         for (Person p : matchedInThisRound) {
             if (potentialMatches.get(p).size() == 0) {
                 stillMatching.remove(p);
@@ -69,8 +83,14 @@ public class Matcher {
         return matches;
     }
 
-    private void createRandomMatches(Set<Person> randomPool,
-                                     Collection<OneOnOne> matches) {
+    /**
+     * Creates random matches within a set of people - will make as many matches as possible. If the set contains an
+     * number of people, one person will not have a matching.
+     *
+     * @param randomPool set of employees (Person)
+     * @param matches collection of matches - any match made will be added to this
+     */
+    private void createRandomMatches(Set<Person> randomPool, Collection<OneOnOne> matches) {
         while (randomPool.size() > 1) {
             Person one = getRandomElement(randomPool);
             randomPool.remove(one);
@@ -82,6 +102,10 @@ public class Matcher {
         }
     }
 
+    /**
+     * Resets global variables back to initial conditions (conditions of the company before any pairings have been
+     * assigned).
+     */
     public void resetMatchingData() {
         stillMatching = new HashSet<Person>(organization.getEmployees());
         done = new HashSet<Person>();
@@ -95,9 +119,13 @@ public class Matcher {
             potentialMatches.put(p, potentialMatchesSet);
             alreadyMatched.put(p, new HashSet<Person>());
         }
-        System.out.println("Matching data reset");
     }
 
+    /**
+     * Gets a random person from a set of Person
+     * @param s set of Person
+     * @return a random Person from the set
+     */
     public Person getRandomElement(Set<Person> s) {
         int index = new Random().nextInt(s.size());
         Iterator<Person> iter = s.iterator();
@@ -107,6 +135,16 @@ public class Matcher {
         return iter.next();
     }
 
+    /**
+     * Creates a match between a Person and a teammate (who has not been assigned a match yet in this round). If no
+     * such match is possible, the Person is matched with null. If a successful matching has occurred, update the set
+     * of all people who have been matched in this round and the global matching tables (potentialMatches and
+     * alreadyMatched).
+     *
+     * @param gettingMatched Person getting matched
+     * @param matched set of people who have been matched already in this round
+     * @return a matching (OneOnOne)
+     */
     public OneOnOne createMatchWithUnmatchedTeammate(Person gettingMatched, Set<Person> matched) {
         Set<Person> possibleMatches = getPossibleMatches(gettingMatched, matched);
         Person matchedWith = possibleMatches.size() > 0 ? getRandomElement(possibleMatches) : null;
@@ -123,6 +161,10 @@ public class Matcher {
         return match;
     }
 
+    /**
+     * Updates the global matching tables (potentialMatches and alreadyMatched) for a given matching
+     * @param o the matching (OneOnOne)
+     */
     private void updateMatchingTables(OneOnOne o) {
         Person p1 = o.x;
         Person p2 = o.y;
@@ -134,6 +176,15 @@ public class Matcher {
         alreadyMatched.get(p2).add(p1);
     }
 
+    /**
+     * Gets possible matches for a Person, given the people who have already been matched. A possible matching is
+     * defined to be any possible teammate with whom the Person has not matched with yet and someone who has not
+     * already been assigned a matching fo r this round.
+     *
+     * @param gettingMatched
+     * @param matched
+     * @return
+     */
     private Set<Person> getPossibleMatches(Person gettingMatched, Set<Person> matched) {
         Set<Person> possible = new HashSet<Person>(potentialMatches.get(gettingMatched));
         possible.removeAll(matched);
